90bdedb67aa2cb67e41f54132f0aa0dc739eb12f
R3kki <richie.parkie@gmail.com>
2020-06-02 17:36:34 -0400

    added: error messages to std error

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1); // non-zero exits means program exited with error
     });
 
     // do not need `unwrap_or_else` since return value is ()
     if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
