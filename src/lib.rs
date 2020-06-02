458b8b661da485190e83cac29eb2747d0939b46b
R3kki <richie.parkie@gmail.com>
2020-06-02 17:10:42 -0400

    search implemented

--- src/lib.rs
+++ src/lib.rs
@@ -19,10 +19,43 @@ impl Config {
     }
 }
 
+// lifetime parameters specify which argument lifetime is connected to the return value
+// i.e. data returned by function will live as long as the `contents` argument
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("Found text:\n{}", contents);
-
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
+}
+/* TDD Test Driven Development
+1. Write a test that fails and run it to make sure it fails for the reason you expect.
+2. Write or modify just enough code to make the new test pass.
+3. Refactor the code you just added or changed and make sure the tests continue to pass.
+4. Repeat from step 1!
+ */
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
